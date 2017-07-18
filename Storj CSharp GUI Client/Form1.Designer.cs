918ebf42153f56b724387b44fbbdc5794898be35
Christopher Thomas Nicodemus <GitHub@niconet2k.com>
2017-07-18 12:36:55 -0400

    Some testing code

--- Storj CSharp GUI Client/Form1.Designer.cs
+++ Storj CSharp GUI Client/Form1.Designer.cs
@@ -28,21 +28,47 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.txtNodeID = new System.Windows.Forms.TextBox();
+			this.btnNodeInfo = new System.Windows.Forms.Button();
 			this.SuspendLayout();
+			// 
+			// txtNodeID
+			// 
+			this.txtNodeID.Location = new System.Drawing.Point(71, 66);
+			this.txtNodeID.Name = "txtNodeID";
+			this.txtNodeID.Size = new System.Drawing.Size(170, 20);
+			this.txtNodeID.TabIndex = 0;
+			this.txtNodeID.Text = "8bcd2636cc4c3799ba4e2e86f45e89bc7ffe05ca";
+			// 
+			// btnNodeInfo
+			// 
+			this.btnNodeInfo.Location = new System.Drawing.Point(122, 117);
+			this.btnNodeInfo.Name = "btnNodeInfo";
+			this.btnNodeInfo.Size = new System.Drawing.Size(127, 28);
+			this.btnNodeInfo.TabIndex = 1;
+			this.btnNodeInfo.Text = "Get Node Info";
+			this.btnNodeInfo.UseVisualStyleBackColor = true;
+			this.btnNodeInfo.Click += new System.EventHandler(this.btnNodeInfo_Click);
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.Controls.Add(this.btnNodeInfo);
+			this.Controls.Add(this.txtNodeID);
 			this.Name = "Form1";
 			this.Text = "Form1";
 			this.Load += new System.EventHandler(this.Form1_Load);
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.TextBox txtNodeID;
+		private System.Windows.Forms.Button btnNodeInfo;
 	}
 }
 
--- Storj CSharp GUI Client/Form1.cs
+++ Storj CSharp GUI Client/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -20,6 +21,16 @@ namespace Storj_CSharp_GUI_Client
 		private void Form1_Load(object sender, EventArgs e)
 		{
 			int r = new Random().Next();
+			//var rest = new REST.RESTProcessor;
+			//REST.RESTProcessor.("https://api.storj.io/contacts/8bcd2636cc4c3799ba4e2e86f45e89bc7ffe05ca","");
+			
+		}
+
+		private void btnNodeInfo_Click(object sender, EventArgs e)
+		{
+		var res = REST.RESTProcessor.GET("https://api.storj.io/contacts/" + txtNodeID.Text);
+		var res2 = JsonConvert.DeserializeObject(res);
+
 		}
 	}
 }
