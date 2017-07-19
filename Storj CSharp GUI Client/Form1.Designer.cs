20063944a800a7bc2b580a20e301328fd25b3847
Christopher Thomas Nicodemus <GitHub@niconet2k.com>
2017-07-19 05:23:41 -0400

    Some code ECDSA generation, got to be something better.

--- Storj CSharp GUI Client/Form1.Designer.cs
+++ Storj CSharp GUI Client/Form1.Designer.cs
@@ -30,6 +30,11 @@
 		{
 			this.txtNodeID = new System.Windows.Forms.TextBox();
 			this.btnNodeInfo = new System.Windows.Forms.Button();
+			this.txtEmail = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.txtPassword = new System.Windows.Forms.TextBox();
+			this.btnCreateUser = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
 			// txtNodeID
@@ -50,11 +55,59 @@
 			this.btnNodeInfo.UseVisualStyleBackColor = true;
 			this.btnNodeInfo.Click += new System.EventHandler(this.btnNodeInfo_Click);
 			// 
+			// txtEmail
+			// 
+			this.txtEmail.Location = new System.Drawing.Point(71, 182);
+			this.txtEmail.Name = "txtEmail";
+			this.txtEmail.Size = new System.Drawing.Size(133, 20);
+			this.txtEmail.TabIndex = 2;
+			this.txtEmail.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(71, 169);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(31, 13);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "email";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(71, 204);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(52, 13);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "password";
+			// 
+			// txtPassword
+			// 
+			this.txtPassword.Location = new System.Drawing.Point(71, 217);
+			this.txtPassword.Name = "txtPassword";
+			this.txtPassword.Size = new System.Drawing.Size(133, 20);
+			this.txtPassword.TabIndex = 4;
+			// 
+			// btnCreateUser
+			// 
+			this.btnCreateUser.Location = new System.Drawing.Point(123, 244);
+			this.btnCreateUser.Name = "btnCreateUser";
+			this.btnCreateUser.Size = new System.Drawing.Size(139, 52);
+			this.btnCreateUser.TabIndex = 6;
+			this.btnCreateUser.Text = "Create User";
+			this.btnCreateUser.UseVisualStyleBackColor = true;
+			this.btnCreateUser.Click += new System.EventHandler(this.btnCreateUser_Click);
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.ClientSize = new System.Drawing.Size(287, 314);
+			this.Controls.Add(this.btnCreateUser);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.txtPassword);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.txtEmail);
 			this.Controls.Add(this.btnNodeInfo);
 			this.Controls.Add(this.txtNodeID);
 			this.Name = "Form1";
@@ -69,6 +122,11 @@
 
 		private System.Windows.Forms.TextBox txtNodeID;
 		private System.Windows.Forms.Button btnNodeInfo;
+		private System.Windows.Forms.TextBox txtEmail;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox txtPassword;
+		private System.Windows.Forms.Button btnCreateUser;
 	}
 }
 
--- Storj CSharp GUI Client/Form1.cs
+++ Storj CSharp GUI Client/Form1.cs
@@ -1,4 +1,8 @@
 ﻿using Newtonsoft.Json;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Generators;
+using Org.BouncyCastle.Security;
+using Org.BouncyCastle.Crypto.Parameters;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -8,6 +12,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using Org.BouncyCastle.OpenSsl;
 
 namespace Storj_CSharp_GUI_Client
 {
@@ -28,9 +34,48 @@ namespace Storj_CSharp_GUI_Client
 
 		private void btnNodeInfo_Click(object sender, EventArgs e)
 		{
-		var res = REST.RESTProcessor.GET("https://api.storj.io/contacts/" + txtNodeID.Text);
+		var res = REST.GET("https://api.storj.io/contacts/" + txtNodeID.Text);
 		var res2 = JsonConvert.DeserializeObject(res);
 
+		}
+
+
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void btnCreateUser_Click(object sender, EventArgs e)
+		{
+			AsymmetricCipherKeyPair keys = GenerateKeys(256);
+			//var pub = (ECPublicKeyParameters)keys.Public;
+			//var prv = (ECPrivateKeyParameters)keys.Private;
+			
+			String pub = getKey(keys.Public).Replace("-","");
+			String prv = getKey(keys.Private).Replace("-",""); //BitConverter.ToString(data);
+
+		}
+		private static string getKey(dynamic key)
+		{
+			TextWriter textWriter = new StringWriter();
+			PemWriter pemWriter = new PemWriter(textWriter);
+			pemWriter.WriteObject(key);
+			pemWriter.Writer.Flush();
+			string pem = textWriter.ToString();
+			var pem2 = pem.Split('\r').Skip(1).TakeWhile(i => !i.Contains("-----")).ToArray();
+			pem = string.Join("",pem2);
+			byte[] bytes = Convert.FromBase64String(pem);
+			string hex = BitConverter.ToString(bytes);
+			return hex;
+		}
+		private static AsymmetricCipherKeyPair GenerateKeys(int keySize)
+		{
+			ECKeyPairGenerator gen = new ECKeyPairGenerator("ECDSA");
+			SecureRandom secureRandom = new SecureRandom();
+			KeyGenerationParameters keyGenParam = new KeyGenerationParameters(secureRandom, keySize);
+			gen.Init(keyGenParam);
+			return gen.GenerateKeyPair();
 		}
 	}
 }
